4c4bd4b9896fd2af2977bc939f2aaf3e7ddac013
ChaseGreen98 <cgreen@basecampcodingacedemy.org>
2026-04-07 17:48:16 -0500

    Start of pagination code in views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpRequest
+from django.contrib.auth import get_user_model
+from app.models import *
 
-# Create your views here.
+User = get_user_model()
+
+def menu_view(request):
+    items = Menu_Item.objects.all()
+
+    paginator = Paginator(items, 4)
+    page_obj = paginator.get_page(request.GET.get("page"))
+
+def merch_view(request):
+    items = Merch_Item.objects.all()
+
+    paginator = Paginator(items, 4)
+    page_obj = paginator.get_page(request.GET.get("page"))
