c66c83e475e665c813b7b6f1cb168c64738e59fb
Chase Green <chasegreen827@gmail.com>
2026-04-27 09:11:58 -0500

    Switch to database configuration using dj_database_url

--- starbright/settings.py
+++ starbright/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,10 +65,7 @@ WSGI_APPLICATION = "starbright.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
